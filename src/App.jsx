702e1a41ba0d9c3ad897f5828f5573384279c4b2
linettej <linettejvalloor@gmail.com>
2023-09-11 10:38:56 +0530

    modified commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import MyModal from "./components/modal";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div class="shrink-0">
+          <img class="h-12 w-12" src="/assets/images.png" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div>
+
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+          </div>
+        </div>
+      </div>
+      <MyModal/>
+    </>
+  );
+}
+
+export default App;
